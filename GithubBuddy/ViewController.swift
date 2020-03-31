f1537dc7aa64f7d9035eadc239f91fa09e65550b
Shayanzahid <shayanzahid0@gmail.com>
2020-04-01 01:10:20 +0500

    Initial Commit

---
+++ GithubBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubBuddy
+//
+//  Created by Shayan Zahid on 01/04/2020.
+//  Copyright © 2020 Shayan Zahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
